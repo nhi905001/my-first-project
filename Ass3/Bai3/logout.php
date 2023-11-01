d1f00e1d4fb9883bc95686359495f2e4dff1888b
nhi905001 <143697597+nhi905001@users.noreply.github.com>
2023-11-01 16:42:42 +0700

    Delete Ass3 directory

--- Ass3/Bai3/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-session_destroy();
-setcookie('username', '', time() - 1);
-setcookie('password', '', time() - 1);
-echo "<script>alert('Logout Successfull.'); location.href='dangnhap.php';</script>";
-?>
